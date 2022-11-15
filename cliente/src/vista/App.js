3823e6008a7ece118e70522bf7c60914977c6d75
Absdag <62969849+Absdag@users.noreply.github.com>
2022-11-14 21:16:57 -0500

    Base creado

---
+++ cliente/src/vista/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import BarraLateral from './BarraLateral';
+
+function App() {
+  return (
+    <div>
+      <div className='app'>
+
+        <div className='app_body'> 
+
+          {/*BARRA LATERAL
+          
+          */}
+          <BarraLateral> </BarraLateral>
+
+          {/*CHAT*/}
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
